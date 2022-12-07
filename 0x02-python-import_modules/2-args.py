e03146a82f825fdcf7d80d4b63894c5986b5e389
Cindy-Onwonga <cindykerubo@gmail.com>
2022-12-07 00:16:53 -0800

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ from sys import argv
 if __name__ == "__main__":
     count = len(argv) - 1
     if count == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments".format(count))
+        print("{} arguments:".format(count))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
 
