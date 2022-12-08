8c3f0a584920b52b92119e848bb0e1192fa23fd7
Cindy-Onwonga <cindykerubo@gmail.com>
2022-12-08 03:38:41 -0800

    dynamic scripts

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv[1:])
+    arg = sys.argv[1:]
+    count = len(arg)
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
         for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i]))
+            print("{}: {}".format(i + 1, arg[i]))
     else:
         print("{} arguments:".format(count))
         for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i]))
+            print("{}: {}".format(i + 1, arg[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    import sys
     sum = 0
     count = len(sys.argv) - 1
     for i in range(count):
