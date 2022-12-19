7c6458ca7ce40cb7ec2724eda7eb8332a37d52f0
Cindy-Onwonga <cindykerubo@gmail.com>
2022-12-19 01:05:02 -0800

    print integer matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            if j == i[-1]:
-                print("{:d}".format(j))
-            else:
-                print(" ")
-        print()
+    if matrix:
+        for i in matrix:
+            for j in i:
+                if j == i[-1]:
+                    print("{:d}".format(j))
+                else:
+                    print(" ")
+            print()
