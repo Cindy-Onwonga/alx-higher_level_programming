82545b916c639e6c4b62f8b5fb36c7b8250f11f2
Cindy-Onwonga <cindykerubo@gmail.com>
2023-03-06 22:49:21 -0800

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,4 +9,21 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *head, *tail;
+
+	if (list == NULL)
+		return (0);
+
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
+	{
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
+			return (1);
+	}
+	return (0);
 }
