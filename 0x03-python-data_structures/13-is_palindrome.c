7d728684e3764d5e73be4ae47fdcba72f7e51f84
Cindy-Onwonga <cindykerubo@gmail.com>
2023-03-19 23:41:32 -0700

    is singly linked list palindrome?

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,4 +11,29 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *node;
+	int values[9999], i = 0, c = 0;
+
+	if ((!*head) || (!head))
+		return (1);
+
+	node = *head;
+	if (!node->next)
+		return (1);
+	while (node)
+	{
+		values[i] = node->n;
+		node = node->next;
+		i++;
+	}
+	i--;
+
+	while (i >= 0 && c <= i)
+	{
+		if (values[i] != values[c])
+			return (0);
+		i--;
+		c++;
+	}
+	return (1);
 }
