d1ef00adb83f6e8f5a0f0a7a67e82e94bc142cb1
Cindy-Onwonga <cindykerubo@gmail.com>
2023-03-19 23:19:14 -0700

    is singly linked list palindrome?

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks if singly linked list is palindrome
+ * @head: head pointer
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+}
